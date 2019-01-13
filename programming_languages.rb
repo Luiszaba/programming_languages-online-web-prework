f421779e2c1a713086c474eb9de45e6e944e0e7b
Luis Zabala <luis.zabala076@gmail.com>
2019-01-13 19:11:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  lang_hash = Hash.new(0)
+  languages.each do |style, language_names|
+    language_names.each do |name, language_type|
+      if lang_hash.has_key?(name)
+        lang_hash[name][:style] << style 
+      else
+        lang_hash[name] = language_type
+        lang_hash[name][:style] = [style]
+      end 
+    end
+  end
+  lang_hash
+end
 end
